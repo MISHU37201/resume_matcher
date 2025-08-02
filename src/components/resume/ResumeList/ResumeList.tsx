5ddfe46f79956226bba74aea1d4c191de4075076
MISHU37201 <147590773+MISHU37201@users.noreply.github.com>
2025-08-02 18:19:10 +0500

    resume list component seperate out from app.tsx

---
+++ src/components/resume/ResumeList/ResumeList.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { ResumeMatch } from "../../../types";
+import { ResumeCard } from "../ResumeCard/ResumeCard";
+
+interface ResumeListProps {
+  resumes: ResumeMatch[];
+  selectedResumes: Set<number>;
+  onToggleSelection: (id: number) => void;
+}
+
+export const ResumeList: React.FC<ResumeListProps> = ({
+  resumes,
+  selectedResumes,
+  onToggleSelection,
+}) => {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
+      {resumes.map((resume) => (
+        <ResumeCard
+          key={resume.id}
+          resume={resume}
+          isSelected={selectedResumes.has(resume.id)}
+          onToggleSelection={() => onToggleSelection(resume.id)}
+        />
+      ))}
+    </div>
+  );
+};
